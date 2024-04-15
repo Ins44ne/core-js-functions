945dca4872fa3590ebe72b80b7ed325a8c96d642
Ins44ne <ins44ne91@gmail.com>
2024-04-15 11:04:14 +0300

    getPowerFunction & getPolynom & memoize done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -74,8 +74,11 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  const powerFunction = (x) => {
+    return x ** exponent;
+  };
+  return powerFunction;
 }
 
 /**
@@ -91,8 +94,18 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coefficients) {
+  if (coefficients.length === 0) {
+    return null;
+  }
+
+  return (x) => {
+    let result = 0;
+    for (let i = 0; i < coefficients.length; i += 1) {
+      result += coefficients[i] * x ** (coefficients.length - 1 - i);
+    }
+    return result;
+  };
 }
 
 /**
@@ -109,8 +122,17 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+
+  function memoizedFunction(...args) {
+    if (!cache) {
+      cache = { result: func(...args) };
+    }
+    return cache.result;
+  }
+
+  return memoizedFunction;
 }
 
 /**
